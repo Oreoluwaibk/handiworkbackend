0e9a186601bca9c6541088b7d2e0163c0a30cf67
Ibikunle Oreoluwa <ore.ibikunle98@gmail.com>
2025-10-17 14:42:13 +0100

    add send grid

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,9 +1,14 @@
 import nodemailer, { Transporter } from 'nodemailer';
 import hbs from 'nodemailer-express-handlebars';
 import path from 'path';
+import fs from 'fs-extra';
+import Handlebars from 'handlebars';
+import sgMail from '@sendgrid/mail';
+
 
 // Define transporter type
 let transporter: Transporter;
+sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 
 // Create transporter
 transporter = nodemailer.createTransport({
@@ -28,18 +33,19 @@ transporter.use(
 
 // Define function to send welcome email
 export async function sendWelcomeEmail(userEmail: string, userName: string): Promise<void> {
-  const mailOptions = {
-    from: '"QuikWrk Admin" <your-email@gmail.com>',
+  const template = await fs.readFile('src/views/welcome.handlebars', 'utf-8');
+  const compiled = Handlebars.compile(template);
+  const html = compiled({ name: userName,  });
+
+  const msg = {
     to: userEmail,
-    subject: 'Welcome!',
-    template: 'welcome', // matches views/welcome.handlebars
-    context: {
-      name: userName,
-    },
+    from: process.env.FROM_EMAIL as string,
+    subject: 'Welcome to QuikWrk!',
+    html,
   };
 
   try {
-    await transporter.sendMail(mailOptions);
+    await sgMail.send(msg);
     console.log(`✅ Email sent to ${userEmail}`);
   } catch (error) {
     console.error(`❌ Failed to send email to ${userEmail}:`, error);
@@ -47,24 +53,47 @@ export async function sendWelcomeEmail(userEmail: string, userName: string): Pro
   }
 }
 
-export async function sendOtp(userEmail: string, otp: string | number, username: string): Promise<void> {
-  const mailOptions = {
-    from: '"QuikWrk Admin" <your-email@gmail.com>',
-    to: userEmail,
-    subject: 'Your One-Time Password (OTP)',
-    template: 'otp',
-    context: {
-      name: username,
-      otp: otp,
-      appName: 'QuikWrk Admin'
+export async function sendOtp(userEmail: string, otp: string | number, userName: string): Promise<void> {
+  // const mailOptions = {
+  //   from: '"QuikWrk Admin" <your-email@gmail.com>',
+  //   to: userEmail,
+  //   subject: 'Your One-Time Password (OTP)',
+  //   template: 'otp',
+  //   context: {
+  //     name: username,
+  //     otp: otp,
+  //     appName: 'QuikWrk Admin'
+  //   }
+  // };
+
+  // try {
+    
+  //   await transporter.sendMail(mailOptions);
+  //   console.log(`✅ Email sent to ${userEmail}`);
+  // } catch (error) {
+  //   console.error(`❌ Failed to send email to ${userEmail}:`, error);
+  //   throw error;
+  // }
+  
+   const template = await fs.readFile('src/views/otp.handlebars', 'utf-8');
+    const compiled = Handlebars.compile(template);
+    const html = compiled({ name: userName, otp: otp, appName: 'QuikWrk Admin' });
+
+    const msg = {
+      to: userEmail,
+      from: process.env.FROM_EMAIL as string,
+      subject: 'Your One-Time Password (OTP)',
+      html,
+    };
+
+    try {
+      await sgMail.send(msg);
+      console.log(`✅ Email sent to ${userEmail}`);
+    } catch (error) {
+      console.error(`❌ Failed to send email to ${userEmail}:`, error);
+      throw error;
     }
-  };
-
-  try {
-    await transporter.sendMail(mailOptions);
-    console.log(`✅ Email sent to ${userEmail}`);
-  } catch (error) {
-    console.error(`❌ Failed to send email to ${userEmail}:`, error);
-    throw error;
-  }
 }
+
+
+
